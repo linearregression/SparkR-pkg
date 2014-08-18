563bd1121ed1ac9e78c374db894c14e94473374f
root <edward>
2014-08-17 21:54:59 -0700

    merge

---
+++ SparkR/tests/test_rdd.R
@@ -0,0 +1,147 @@
+context("basic RDD functions")
+
+# JavaSparkContext handle
+sc <- sparkR.init()
+
+# Data
+nums <- 1:10
+rdd <- parallelize(sc, nums, 2L)
+
+test_that("count and length on RDD", {
+   expect_equal(count(rdd), 10)
+   expect_equal(length(rdd), 10)
+})
+
+test_that("lapply on RDD", {
+  multiples <- lapply(rdd, function(x) { 2 * x })
+  actual <- collect(multiples)
+  expect_equal(actual, as.list(nums * 2))
+})
+
+test_that("lapplyPartition on RDD", {
+  sums <- lapplyPartition(rdd, function(part) { sum(unlist(part)) })
+  actual <- collect(sums)
+  expect_equal(actual, list(15, 40))
+})
+
+test_that("several transformations on RDD (a benchmark on PipelinedRDD)", {
+  rdd2 <- rdd
+  for (i in 1:12)
+    rdd2 <- lapplyPartitionsWithIndex(
+              rdd2, function(split, part) {
+                part <- as.list(unlist(part) * split + i)
+              })
+  rdd2 <- lapply(rdd2, function(x) x + x)
+  collect(rdd2)
+})
+
+test_that("PipelinedRDD support actions: cache(), unpersist(), checkpoint()", {
+  # RDD
+  rdd2 <- rdd
+  # PipelinedRDD
+  rdd2 <- lapplyPartitionsWithIndex(
+            rdd2,
+            function(split, part) {
+              part <- as.list(unlist(part) * split)
+            })
+
+  cache(rdd2)
+  expect_true(rdd2@env$isCached)
+  rdd2 <- lapply(rdd2, function(x) x)
+  expect_false(rdd2@env$isCached)
+
+  unpersist(rdd2)
+  expect_false(rdd2@env$isCached)
+
+  setCheckpointDir(sc, "checkpoints")
+  checkpoint(rdd2)
+  expect_true(rdd2@env$isCheckpointed)
+
+  rdd2 <- lapply(rdd2, function(x) x)
+  expect_false(rdd2@env$isCached)
+  expect_false(rdd2@env$isCheckpointed)
+
+  # make sure the data is collectable
+  collect(rdd2)
+
+  unlink("checkpoints")
+})
+
+test_that("reduce on RDD", {
+  sum <- reduce(rdd, "+")
+  expect_equal(sum, 55)
+
+  # Also test with an inline function
+  sumInline <- reduce(rdd, function(x, y) { x + y })
+  expect_equal(sumInline, 55)
+})
+
+test_that("lapply with dependency", {
+  fa <- 5
+  multiples <- lapply(rdd, function(x) { fa * x })
+  actual <- collect(multiples)
+
+  expect_equal(actual, as.list(nums * 5))
+})
+
+test_that("lapplyPartitionsWithIndex on RDDs", {
+  func <- function(splitIndex, part) { list(splitIndex, Reduce("+", part)) }
+  actual <- collect(lapplyPartitionsWithIndex(rdd, func), flatten = FALSE)
+  expect_equal(actual, list(list(0, 15), list(1, 40)))
+
+  pairsRDD <- parallelize(sc, list(list(1, 2), list(3, 4), list(4, 8)), 1L)
+  partitionByParity <- function(key) { if (key %% 2 == 1) 0 else 1 }
+  mkTup <- function(splitIndex, part) { list(splitIndex, part) }
+  actual <- collect(lapplyPartitionsWithIndex(
+                      partitionBy(pairsRDD, 2L, partitionByParity),
+                      mkTup),
+                    FALSE)
+  expect_equal(actual, list(list(0, list(list(1, 2), list(3, 4))),
+                            list(1, list(list(4, 8)))))
+})
+
+test_that("sampleRDD() on RDDs", {
+  expect_equal(unlist(collect(sampleRDD(rdd, FALSE, 1.0, 2014L))), nums)
+})
+
+test_that("takeSample() on RDDs", {
+  # ported from RDDSuite.scala, modified seeds
+  data <- parallelize(sc, 1:100, 2L)
+  for (seed in 4:5) {
+    s <- takeSample(data, FALSE, 20L, seed)
+    expect_equal(length(s), 20L)
+    expect_equal(length(unique(s)), 20L)
+    for (elem in s) {
+      expect_true(elem >= 1 && elem <= 100)
+    }
+  }
+  for (seed in 4:5) {
+    s <- takeSample(data, FALSE, 200L, seed)
+    expect_equal(length(s), 100L)
+    expect_equal(length(unique(s)), 100L)
+    for (elem in s) {
+      expect_true(elem >= 1 && elem <= 100)
+    }
+  }
+  for (seed in 4:5) {
+    s <- takeSample(data, TRUE, 20L, seed)
+    expect_equal(length(s), 20L)
+    for (elem in s) {
+      expect_true(elem >= 1 && elem <= 100)
+    }
+  }
+  for (seed in 4:5) {
+    s <- takeSample(data, TRUE, 100L, seed)
+    expect_equal(length(s), 100L)
+    # Chance of getting all distinct elements is astronomically low, so test we
+    # got < 100
+    expect_true(length(unique(s)) < 100L)
+  }
+  for (seed in 4:5) {
+    s <- takeSample(data, TRUE, 200L, seed)
+    expect_equal(length(s), 200L)
+    # Chance of getting all distinct elements is still quite low, so test we
+    # got < 100
+    expect_true(length(unique(s)) < 100L)
+  }
+})
---
+++ SparkR/worker/worker.R
@@ -0,0 +1,127 @@
+# Worker class
+
+source_local <- function(fname) {
+  argv <- commandArgs(trailingOnly = FALSE)
+  base_dir <- dirname(substring(argv[grep("--file=", argv)], 8))
+  source(paste(base_dir, fname, sep="/"))
+}
+
+source_local("serialize.R")
+
+# NOTE: We use "stdin" to get the process stdin instead of the command line
+inputCon  <- file("stdin", open = "rb")
+
+outputFileName <- readLines(inputCon, n = 1)
+outputCon <- file(outputFileName, open="wb")
+
+# Set libPaths to include SparkR package as loadNamespace needs this
+# TODO: Figure out if we can avoid this by not loading any objects that require
+# SparkR namespace
+rLibDir <- readLines(inputCon, n = 1)
+.libPaths(c(rLibDir, .libPaths()))
+
+suppressPackageStartupMessages(library(SparkR))
+
+# read the index of the current partition inside the RDD
+splitIndex <- readInt(inputCon)
+
+# read the function; if used for pairwise RRDD, this is the hash function.
+execLen <- readInt(inputCon)
+execFunctionName <- unserialize(readRawLen(inputCon, execLen))
+
+# read the isSerialized bit flag
+isSerialized <- readInt(inputCon)
+
+# Redirect stdout to stderr to prevent print statements from
+# interfering with outputStream
+sink(stderr())
+
+# read function dependencies
+depsLen <- readInt(inputCon)
+if (depsLen > 0) {
+  execFunctionDeps <- readRawLen(inputCon, depsLen)
+
+  # load the dependencies into current environment
+  depsFileName <- tempfile(pattern="spark-exec", fileext=".deps")
+  depsFile <- file(depsFileName, open="wb")
+  writeBin(execFunctionDeps, depsFile, endian="big")
+  close(depsFile)
+
+  load(depsFileName)
+  unlink(depsFileName)
+}
+
+# Include packages as required
+packageNames <- unserialize(readRaw(inputCon))
+for (pkg in packageNames) {
+  suppressPackageStartupMessages(require(as.character(pkg), character.only=TRUE))
+}
+
+# Read and set broadcast variables
+numBroadcastVars <- readInt(inputCon)
+if (numBroadcastVars > 0) {
+  for (bcast in seq(1:numBroadcastVars)) {
+    bcastId <- readInt(inputCon)
+    value <- unserialize(readRaw(inputCon))
+    setBroadcastValue(bcastId, value)
+  }
+}
+
+# If -1: read as normal RDD; if >= 0, treat as pairwise RDD and treat the int
+# as number of partitions to create.
+numPartitions <- readInt(inputCon)
+
+isEmpty <- readInt(inputCon)
+
+if (isEmpty != 0) {
+
+  if (numPartitions == -1) {
+    if (isSerialized) {
+      # Now read as many characters as described in funcLen
+      data <- readDeserialize(inputCon)
+    } else {
+      data <- readLines(inputCon)
+    }
+    output <- do.call(execFunctionName, list(splitIndex, data))
+    writeRaw(outputCon, output)
+  } else {
+    if (isSerialized) {
+      # Now read as many characters as described in funcLen
+      data <- readDeserialize(inputCon)
+    } else {
+      data <- readLines(inputCon)
+    }
+
+    res <- new.env()
+
+    # Step 1: hash the data to an environment
+    hashTupleToEnvir <- function(tuple) {
+      # NOTE: execFunction is the hash function here
+      hashVal <- do.call(execFunctionName, list(tuple[[1]]))
+      bucket <- as.character(hashVal %% numPartitions)
+      acc <- res[[bucket]]
+      # TODO?: http://stackoverflow.com/questions/2436688/append-an-object-to-a-list-in-r-in-amortized-constant-time
+      acc[[length(acc) + 1]] <- tuple
+      res[[bucket]] <- acc
+    }
+    invisible(lapply(data, hashTupleToEnvir))
+
+    # Step 2: write out all of the environment as key-value pairs.
+    for (name in ls(res)) {
+      writeInt(outputCon, 2L)
+      writeInt(outputCon, as.integer(name))
+      writeRaw(outputCon, res[[name]])
+    }
+  }
+}
+
+# End of output
+writeInt(outputCon, 0L)
+
+close(outputCon)
+
+# Restore stdout
+sink()
+
+# Finally print the name of the output file
+cat(outputFileName, "\n")
